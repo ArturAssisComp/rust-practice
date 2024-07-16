bf5c6f097dfaa8a31da2ac5d7a92610662e573a2
Artur Assis Alves <arturassiscomp@gmail.com>
2024-07-16 14:52:18 -0300

    implement the lib

--- projects/todo_list_parser/src/error.rs
+++ projects/todo_list_parser/src/error.rs
@@ -1,9 +1,10 @@
 use std::error::Error;
+use std::fmt;
 use std::fmt::Display;
 
 #[derive(Debug)]
 pub enum ParseError {
-    Malformed,
+    //Malformed,
     Empty,
 }
 
@@ -15,5 +16,29 @@ pub struct ReadError {
 impl Display for ReadError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "Failed reading todo file")
+    }
+}
+
+impl Error for ReadError {
+    fn description(&self) -> &str {
+        "Todo list read failed!"
+    }
+    fn cause(&self) -> Option<&dyn Error> {
+        Some(&*self.child_error)
+    }
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Failed parsing todo file")
+    }
+}
+
+impl Error for ParseError {
+    fn description(&self) -> &str {
+        "Todo list parse failed!"
+    }
+    fn cause(&self) -> Option<&dyn Error> {
+        None
     }
 }
--- projects/todo_list_parser/src/lib.rs
+++ projects/todo_list_parser/src/lib.rs
@@ -1,5 +1,51 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+//! This crate provides an API to parse a list of todos
+
+use std::fs::read_to_string;
+use std::path::Path;
+
+mod error;
+use error::{ParseError, ReadError};
+
+use std::error::Error;
+
+#[derive(Debug)]
+pub struct TodoList {
+    tasks: Vec<String>,
+}
+
+impl TodoList {
+    pub fn get_todos<P>(path: P) -> Result<TodoList, Box<dyn Error>>
+    where
+        P: AsRef<Path>,
+    {
+        let read_todos: Result<String, Box<dyn Error>> = read_todos(path);
+        let parsed_todos = parse_todos(&read_todos?)?;
+        Ok(parsed_todos)
+    }
+}
+
+pub fn read_todos<P>(path: P) -> Result<String, Box<dyn Error>>
+where
+    P: AsRef<Path>,
+{
+    let raw_todos = read_to_string(path).map_err(|e| ReadError {
+        child_error: Box::new(e),
+    })?;
+    Ok(raw_todos)
+}
+
+pub fn parse_todos(todo_str: &str) -> Result<TodoList, Box<dyn Error>> {
+    let mut tasks: Vec<String> = vec![];
+
+    for line in todo_str.lines() {
+        tasks.push(line.to_string());
+    }
+
+    if tasks.is_empty() {
+        Err(ParseError::Empty.into())
+    } else {
+        Ok(TodoList { tasks: tasks })
+    }
 }
 
 #[cfg(test)]
