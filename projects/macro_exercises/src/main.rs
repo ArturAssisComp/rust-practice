b2f6cb20390720212afa993887815c039863ac73
Artur Assis Alves <arturassiscomp@gmail.com>
2024-07-30 15:10:35 -0300

    solve problems 1 and 2

--- projects/macro_exercises/src/main.rs
+++ projects/macro_exercises/src/main.rs
@@ -1,3 +1,20 @@
+macro_rules! hello_parser {
+    (HELLO $string:tt) => {
+        concat!("HELLO ", stringify!($string))
+    };
+    (HELLO $string:tt!) => {
+        concat!("HELLO ", stringify!($string), "!")
+    };
+}
+
+macro_rules! unordered_html_list {
+    ($($element:expr), *) => {
+        concat!("<ul>\n", $(concat!("\t<li>", $element, "</li>\n"), )* "</ul>")
+    };
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!(hello_parser!(HELLO world));
+    println!(unordered_html_list!(1, 2, 3));
+    println!(unordered_html_list!("element 1", 'h', 123));
 }
