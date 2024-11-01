1b4bb5114937494f78a1e2e7363ca7a1d1714425
Artur Assis Alves <arturassiscomp@gmail.com>
2024-11-01 11:14:54 -0300

    add search functionality

--- projects/minigrep/src/lib.rs
+++ projects/minigrep/src/lib.rs
@@ -30,7 +30,80 @@ impl<'a> ParsedMainArgs<'a> {
 
 pub fn run(parsed_main_args: &ParsedMainArgs) -> Result<(), Box<dyn Error>> {
     let file_content = fs::read_to_string(parsed_main_args.file_path)?;
-    dbg!(file_content);
-
+    for (n, found_line) in search(parsed_main_args.query(), &file_content) {
+        println!("({}): \"{found_line}\"", n + 1);
+    }
     Ok(())
+}
+
+fn search<'a>(query: &'a str, contents: &'a str) -> impl Iterator<Item = (usize, &'a str)> {
+    contents
+        .lines()
+        .enumerate()
+        .filter(move |(_, line)| line.contains(query))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod search {
+        use super::*;
+
+        #[test]
+        fn should_match_return_search() {
+            let test_cases = [
+                (
+                    "Should return empty result",
+                    "not present",
+                    concat!("Line 1 \n", "Line 2\n"),
+                    Vec::<&str>::new(),
+                ),
+                (
+                    "Should return empty result (query: \\n)",
+                    "\n",
+                    concat!("Line 1 \n", "Line 2\n"),
+                    Vec::<&str>::new(),
+                ),
+                (
+                    "Should return empty result (invalid substring)",
+                    "safeduct",
+                    concat!("Rust:\n", "safe, fast, productive.\n", "Pick three.",),
+                    Vec::<&str>::new(),
+                ),
+                (
+                    "Should return one result",
+                    "duct",
+                    concat!("Rust:\n", "safe, fast, productive.\n", "Pick three.",),
+                    vec!["safe, fast, productive."],
+                ),
+                (
+                    "Should return two results",
+                    "duct",
+                    concat!(
+                        "Rust:\n",
+                        "safe, fast, productive.\n",
+                        "Pick three.\n",
+                        " this is a dductt \n"
+                    ),
+                    vec!["safe, fast, productive.", " this is a dductt "],
+                ),
+                (
+                    "Should match everything",
+                    "",
+                    concat!("Rust:\n", "safe, fast, productive.\n", "Pick three.",),
+                    vec!["Rust:", "safe, fast, productive.", "Pick three."],
+                ),
+            ];
+
+            for (description, query, contents, expected_result) in test_cases {
+                assert_eq!(
+                    expected_result,
+                    search(query, contents).collect::<Vec<&str>>(),
+                    "{}",
+                    description
+                );
+            }
+        }
+    }
 }
