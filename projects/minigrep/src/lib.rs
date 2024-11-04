eb4cdc0e94fc47a34525f2d259987d062dfe747f
Artur Assis Alves <arturassiscomp@gmail.com>
2024-11-04 10:29:17 -0300

    implement search case insensitive

--- projects/minigrep/src/lib.rs
+++ projects/minigrep/src/lib.rs
@@ -5,9 +5,6 @@ pub struct ParsedMainArgs<'a> {
     file_path: &'a str,
 }
 impl<'a> ParsedMainArgs<'a> {
-    pub fn query(&self) -> &str {
-        self.query
-    }
     pub fn file_path(&self) -> &str {
         self.file_path
     }
@@ -30,7 +27,7 @@ impl<'a> ParsedMainArgs<'a> {
 
 pub fn run(parsed_main_args: &ParsedMainArgs) -> Result<(), Box<dyn Error>> {
     let file_content = fs::read_to_string(parsed_main_args.file_path)?;
-    for (n, found_line) in search(parsed_main_args.query(), &file_content) {
+    for (n, found_line) in search(parsed_main_args.query, &file_content) {
         println!("({}): \"{found_line}\"", n + 1);
     }
     Ok(())
@@ -41,6 +38,15 @@ fn search<'a>(query: &'a str, contents: &'a str) -> impl Iterator<Item = (usize,
         .lines()
         .enumerate()
         .filter(move |(_, line)| line.contains(query))
+}
+fn search_case_insensitive<'a>(
+    query: &'a str,
+    contents: &'a str,
+) -> impl Iterator<Item = (usize, &'a str)> {
+    contents
+        .lines()
+        .enumerate()
+        .filter(move |(_, line)| line.to_lowercase().contains(query.to_lowercase().as_str()))
 }
 
 #[cfg(test)]
@@ -89,6 +95,17 @@ mod tests {
                     vec!["safe, fast, productive.", " this is a dductt "],
                 ),
                 (
+                    "Should return one result (case sensitive)",
+                    "Duct",
+                    concat!(
+                        "Rust:\n",
+                        "safe, fast, productive.\n",
+                        "Pick three.\n",
+                        " this is a dDuctt \n"
+                    ),
+                    vec![" this is a dDuctt "],
+                ),
+                (
                     "Should match everything",
                     "",
                     concat!("Rust:\n", "safe, fast, productive.\n", "Pick three.",),
@@ -99,7 +116,76 @@ mod tests {
             for (description, query, contents, expected_result) in test_cases {
                 assert_eq!(
                     expected_result,
-                    search(query, contents).collect::<Vec<&str>>(),
+                    search(query, contents).map(|v| v.1).collect::<Vec<&str>>(),
+                    "{}",
+                    description
+                );
+            }
+        }
+
+        #[test]
+        fn should_match_return_search_case_insensitive() {
+            let test_cases = [
+                (
+                    "Should return empty result",
+                    "not present",
+                    concat!("Line 1 \n", "Line 2\n"),
+                    Vec::<&str>::new(),
+                ),
+                (
+                    "Should return empty result (query: \\n)",
+                    "\n",
+                    concat!("Line 1 \n", "Line 2\n"),
+                    Vec::<&str>::new(),
+                ),
+                (
+                    "Should return empty result (invalid substring)",
+                    "safeduct",
+                    concat!("Rust:\n", "safe, fast, productive.\n", "Pick three.",),
+                    Vec::<&str>::new(),
+                ),
+                (
+                    "Should return one result",
+                    "duct",
+                    concat!("Rust:\n", "safe, fast, productive.\n", "Pick three.",),
+                    vec!["safe, fast, productive."],
+                ),
+                (
+                    "Should return two results",
+                    "duct",
+                    concat!(
+                        "Rust:\n",
+                        "safe, fast, productive.\n",
+                        "Pick three.\n",
+                        " this is a dductt \n"
+                    ),
+                    vec!["safe, fast, productive.", " this is a dductt "],
+                ),
+                (
+                    "Should return two results (case insensitive)",
+                    "DuCt",
+                    concat!(
+                        "Rust:\n",
+                        "safe, fast, prodUctive.\n",
+                        "Pick three.\n",
+                        " this is a dDucTt \n"
+                    ),
+                    vec!["safe, fast, prodUctive.", " this is a dDucTt "],
+                ),
+                (
+                    "Should match everything",
+                    "",
+                    concat!("Rust:\n", "safe, fast, productive.\n", "Pick three.",),
+                    vec!["Rust:", "safe, fast, productive.", "Pick three."],
+                ),
+            ];
+
+            for (description, query, contents, expected_result) in test_cases {
+                assert_eq!(
+                    expected_result,
+                    search_case_insensitive(query, contents)
+                        .map(|v| v.1)
+                        .collect::<Vec<&str>>(),
                     "{}",
                     description
                 );
